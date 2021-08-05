c6142012684257dfce4afc15f917810b50d20669
Jack Zhang <Jack.Zhang@nodus.com>
2021-08-05 11:00:56 +0800

    delete unused code

--- PayFabric/Payment/Model/Source/Cctype.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace PayFabric\Payment\Model\Source;
-class Cctype extends \Magento\Payment\Model\Source\Cctype{
-    public function getAllowedTypes()
-    {
-        return array('VI', 'MC', 'AE', 'DI', 'JCB', 'OT');
-    }
-}
