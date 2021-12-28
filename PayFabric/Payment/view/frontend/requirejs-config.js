a3a20303bb6ca059350f3505f63da434cd1350bf
Jack Zhang <Jack.Zhang@nodus.com>
2021-12-28 14:42:49 +0800

    add config js

---
+++ PayFabric/Payment/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    paths: {
+        "axios": "https://www.payfabric.com/Payment/WebGate/Content/scripts/lib/axios.min"
+    }
+}
