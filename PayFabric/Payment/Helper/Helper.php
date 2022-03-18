e1e11975ee18d25dd2ccbc1d70a12f99fca1eb2b
Jack Zhang <Jack.Zhang@nodus.com>
2022-03-18 10:14:46 +0800

    change plugin version

--- PayFabric/Payment/Helper/Helper.php
+++ PayFabric/Payment/Helper/Helper.php
@@ -321,7 +321,7 @@ class Helper extends AbstractHelper
             "Amount" => $this->formatAmount($quote->getGrandTotal()),
             "Currency" => strtoupper( $quote->getCurrency()->getQuoteCurrencyCode() ),
             "pluginName" => "Magento PayFabric Gateway",
-            "pluginVersion" => "1.1.0",
+            "pluginVersion" => "1.2.0",
             "customerId" => $customerId,
             //level2/3
             'freightAmount'    => $this->formatAmount($shippingAddress->getBaseShippingAmount()),
