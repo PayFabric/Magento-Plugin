e2aa10dbec3eaa0127b97d2d6a56d3cde24c4a34
Jack Zhang <Jack.Zhang@nodus.com>
2022-08-12 15:48:59 +0800

    remove unused code

--- PayFabric/Payment/Controller/Hosted/Request.php
+++ PayFabric/Payment/Controller/Hosted/Request.php
@@ -2,9 +2,6 @@
 
 namespace PayFabric\Payment\Controller\Hosted;
 
-use PayFabric\Payment\Helper\Helper;
-use PayFabric\Payment\Model\PaymentMethod;
-use Symfony\Component\Config\Definition\Exception\Exception;
 class Request extends \PayFabric\Payment\Controller\Checkout
 {
     public function execute()
@@ -36,59 +33,5 @@ class Request extends \PayFabric\Payment\Controller\Checkout
         $this->_quoteRepository->save( $quote );
         $result = $this->getCheckoutHelper()->processPayment( $paymentMethod, $quote );
         die(json_encode($result));
-    }
-
-    public function getFormFields()
-    {
-        $result = [];
-        try {
-            if ($this->_order->getPayment()) {
-                $result = $this->_order->getPayment()->getMethodInstance()->getFormFields();
-            }
-        } catch (Exception $e) {
-            $this->_helper->logDebug('Could not get redirect form fields: '.$e);
-        }
-
-        return $result;
-    }
-
-    public function getMerchantLandingPageUrl(){
-        $result = '';
-        try {
-            $order = $this->_order;
-            if ($order->getPayment()) {
-                $result = $this->_order->getPayment()->getMethodInstance()->getMerchantLandingPageUrl();
-            }
-        } catch (Exception $e) {
-            $this->_helper->logDebug('Could not get MerchantLandingPageUrl: '.$e);
-            throw($e);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Get order object.
-     *
-     * @return \Magento\Sales\Model\Order
-     */
-    private function _getOrder()
-    {
-        if (!$this->_order) {
-            $incrementId = $this->_getCheckout()->getLastRealOrderId();
-            $this->_order = $this->_orderFactory->create()->loadByIncrementId($incrementId);
-        }
-
-        return $this->_order;
-    }
-
-    /**
-     * Get frontend checkout session object.
-     *
-     * @return \Magento\Checkout\Model\Session
-     */
-    private function _getCheckout()
-    {
-        return $this->_checkoutSession;
     }
 }
