b6991a3a5d5ea4b0d3c7e336308442f7ce5614e6
Jack Zhang <Jack.Zhang@nodus.com>
2023-03-24 14:48:44 +0800

    replace out version with standalone v1

--- PayFabric/Payment/view/frontend/requirejs-config.js
+++ PayFabric/Payment/view/frontend/requirejs-config.js
@@ -6,7 +6,7 @@
 var config = {
     paths: {
         "axios": "https://www.payfabric.com/Payment/WebGate/Content/scripts/lib/axios.min",
-        "payfabricpayments": "https://www.payfabric.com/Payment/WebGate/src/output/payfabricpayments",
+        "payfabricpayments": "https://www.payfabric.com/Payment/WebGate/Content/bundles/payfabricpayments.standalone.min",
         "iframeResizer":"https://www.payfabric.com/Payment/WebGate/Content/scripts/lib/iframeResizer.min"
     },
     shim: {
